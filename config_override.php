926b2da57af8766de525acfaf5f1d5f81bce1a4b
root <root@ip-172-31-29-159.ap-south-1.compute.internal>
2019-07-12 12:23:21 +0000

    Fixed untracked files

---
+++ config_override.php
@@ -0,0 +1,34 @@
+<?php
+/***CONFIGURATOR***/
+$sugar_config['disable_persistent_connections'] = false;
+$sugar_config['dbconfigoption']['collation'] = 'utf8_general_ci';
+$sugar_config['default_currency_iso4217'] = '356';
+$sugar_config['developerMode'] = false;
+$sugar_config['default_module_favicon'] = false;
+$sugar_config['dashlet_auto_refresh_min'] = '30';
+$sugar_config['stack_trace_errors'] = false;
+$sugar_config['aos']['lineItems']['enableGroups'] = false;
+$sugar_config['dump_slow_queries'] = true;
+$sugar_config['log_memory_usage'] = true;
+$sugar_config['logger']['level'] = 'error';
+$sugar_config['email_allow_send_as_user'] = true;
+$sugar_config['email_xss'] = 'YToxNDp7czo2OiJhcHBsZXQiO3M6NjoiYXBwbGV0IjtzOjQ6ImJhc2UiO3M6NDoiYmFzZSI7czo1OiJlbWJlZCI7czo1OiJlbWJlZCI7czo0OiJmb3JtIjtzOjQ6ImZvcm0iO3M6NToiZnJhbWUiO3M6NToiZnJhbWUiO3M6ODoiZnJhbWVzZXQiO3M6ODoiZnJhbWVzZXQiO3M6NjoiaWZyYW1lIjtzOjY6ImlmcmFtZSI7czo2OiJpbXBvcnQiO3M6ODoiXD9pbXBvcnQiO3M6NToibGF5ZXIiO3M6NToibGF5ZXIiO3M6NDoibGluayI7czo0OiJsaW5rIjtzOjY6Im9iamVjdCI7czo2OiJvYmplY3QiO3M6NToic3R5bGUiO3M6NToic3R5bGUiO3M6MzoieG1wIjtzOjM6InhtcCI7czo2OiJzY3JpcHQiO3M6Njoic2NyaXB0Ijt9';
+$sugar_config['email_enable_confirm_opt_in'] = 'confirmed-opt-in';
+$sugar_config['hide_history_contacts_emails']['Cases'] = true;
+$sugar_config['hide_history_contacts_emails']['Accounts'] = true;
+$sugar_config['hide_history_contacts_emails']['Opportunities'] = true;
+$sugar_config['disable_count_query'] = true;
+$sugar_config['disable_related_calc_fields'] = true;
+$sugar_config['list_max_enteries_per_page'] = '10';
+$sugar_config['passwordsetting']['SystemGeneratedPasswordON'] = '1';
+$sugar_config['passwordsetting']['forgotpasswordON'] = '1';
+$sugar_config['passwordsetting']['systexpiration'] = '1';
+$sugar_config['passwordsetting']['oneupper'] = '1';
+$sugar_config['passwordsetting']['onelower'] = '1';
+$sugar_config['passwordsetting']['onenumber'] = '1';
+$sugar_config['passwordsetting']['onespecial'] = '1';
+$sugar_config['SAML_loginurl'] = '';
+$sugar_config['SAML_logouturl'] = '';
+$sugar_config['SAML_X509Cert'] = '';
+$sugar_config['authenticationClass'] = '';
+/***CONFIGURATOR***/
